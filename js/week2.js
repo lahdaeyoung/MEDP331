01a0e1b38738c280cc9bc8eb0b23c236153a4b72
lahdaeyoung <lahydaeyoung@gmail.com>
2022-02-21 23:05:42 -0500

    update

--- js/week2.js
+++ js/week2.js
@@ -1,5 +1,5 @@
 var t = 0,
-    material = ['SAND',
+material = ['SAND',
     'DUST',
     '<span style="color:transparent">L</span>EAVES',
     'PAPER',
@@ -16,7 +16,7 @@ var t = 0,
     'WOOD',
     '<span style="color:transparent">ST</span>RAW',
     'WEED<span style="color:transparent">S</span>'],
-    place = ['IN <span style="color:transparent">A GREEN, MOSSY</span> TERRA<span style="color:transparent">IN</span>',
+place = ['IN <span style="color:transparent">A GREEN, MOSSY</span> TERRA<span style="color:transparent">IN</span>',
     'IN A<span style="color:transparent">N OVERPOPU</span>LATE<span style="color:transparent">D</span> AREA',
     'BY THE SEA',
     'BY A<span style="color:transparent">N A</span>BAND<span style="color:transparent">ONED LAKE</span>',
@@ -40,38 +40,38 @@ var t = 0,
     'IN A DESERT<span style="color:transparent">ED</span> CHURCH',
     'IN A METRO<span style="color:transparent">POLIS</span>',
     'UNDERWATER'],
-    light_source = ['<span style="color:transparent">CANDLES</span>',
+light_source = ['<span style="color:transparent">CANDLES</span>',
     'ALL<span style="color:transparent"> AVAILABLE LIGHTING</span>',
     '<span style="color:transparent">ELECTRI</span>CITY',
     '<span style="color:transparent">N</span>A<span style="color:transparent">TURAL </span>LIGHT'],
-    inhabitants = ['PEOPLE WHO SLEEP <span style="color:transparent">VERY LITTLE</span>', 'VEGETARIANS', 'HORSES AND BIRDS', '<span style="color:transparent">PEOPLE SPEAKING MANY</span> LANGUAGES WEARING <span style="color:transparent">LITTLE OR NO</span> CLOTHING', 'ALL RACES <span style="color:transparent">OF MEN</span> REPRESENTED <span style="color:transparent">WEARING PREDOMINANTLY RED CLOTHING</span>', 'CHILDREN AND OLD PEOPLE', 'VARIOUS BIRDS AND FISH', 'LOVERS', 'FISHERMEN AND FAMILIES', 'PEOPLE WHO ENJOY EATING <span style="color:transparent">together</span>', 'PEOPLE WHO EAT A GREAT DEAL', 'COLLECTORS OF ALL TYPES', 'FRIENDS AND ENEMIES', 'PEOPLE WHO <span style="color:transparent">SLEEP ALMOST ALL THE</span> TIME', 'VERY TALL PEOPLE', '<span style="color:transparent">AM</span>ER<span style="color:transparent">ICAN</span> INDIANS', 'LITTLE BOYS', 'PEOPLE <span style="color:transparent">FROM MANY WALKS</span> OF LIFE', '<span style="color:transparent">NEGROS WEARING</span> ALL COLORS', 'FRIENDS', '<span style="color:transparent">FRENCH AND GERMAN</span> SPEAKING PEOPLE', 'PEOPLE WHO LOVE <span style="color:transparent">TO READ</span>'];
-    
-    function rand_range(maximum) {
-        "use strict";
-        return Math.floor(Math.random() * (maximum + 1));
-    }
-    function choose(array) {
-        "use strict";
-        return array[rand_range(array.length - 1)];
-    }
-    function stanza() {
-        "use strict";
-        return '<span style="color:transparent">A HO</span>USE OF ' + choose(material) + '<br \>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + choose(place) + '<br \>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:transparent">U</span>SING ' + choose(light_source) + '<br \>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:transparent">INHABITED</span> BY ' + choose(inhabitants);
-    }
-    function litany() {
-        "use strict";
-        var last, text, main = document.getElementById('main');
-        if (8 > t) {
-            t += 1;
-        } else {
-            main.removeChild(document.getElementById('main').firstChild);
-        }
-        last = document.createElement('div');
-        last.innerHTML = stanza();
-        main.appendChild(last);
-    }
-    function produce_litany() {
-        "use strict";
-        litany();
-        setInterval(litany, 5000);
-    }
+inhabitants = ['PEOPLE WHO SLEEP <span style="color:transparent">VERY LITTLE</span>', 'VEGETARIANS', 'HORSES AND BIRDS', '<span style="color:transparent">PEOPLE SPEAKING MANY</span> LANGUAGES WEARING <span style="color:transparent">LITTLE OR NO</span> CLOTHING', 'ALL RACES <span style="color:transparent">OF MEN</span> REPRESENTED <span style="color:transparent">WEARING PREDOMINANTLY RED CLOTHING</span>', 'CHILDREN AND OLD PEOPLE', 'VARIOUS BIRDS AND FISH', 'LOVERS', 'FISHERMEN AND FAMILIES', 'PEOPLE WHO ENJOY EATING <span style="color:transparent">together</span>', 'PEOPLE WHO EAT A GREAT DEAL', 'COLLECTORS OF ALL TYPES', 'FRIENDS AND ENEMIES', 'PEOPLE WHO <span style="color:transparent">SLEEP ALMOST ALL THE</span> TIME', 'VERY TALL PEOPLE', '<span style="color:transparent">AM</span>ER<span style="color:transparent">ICAN</span> INDIANS', 'LITTLE BOYS', 'PEOPLE <span style="color:transparent">FROM MANY WALKS</span> OF LIFE', '<span style="color:transparent">NEGROS WEARING</span> ALL COLORS', 'FRIENDS', '<span style="color:transparent">FRENCH AND GERMAN</span> SPEAKING PEOPLE', 'PEOPLE WHO LOVE <span style="color:transparent">TO READ</span>'];
+
+function rand_range(maximum) {
+"use strict";
+return Math.floor(Math.random() * (maximum + 1));
+}
+function choose(array) {
+"use strict";
+return array[rand_range(array.length - 1)];
+}
+function stanza() {
+"use strict";
+return '<span style="color:transparent">A HO</span>USE OF ' + choose(material) + '<br \>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + choose(place) + '<br \>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:transparent">U</span>SING ' + choose(light_source) + '<br \>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:transparent">INHABITED</span> BY ' + choose(inhabitants);
+}
+function litany() {
+"use strict";
+var last, text, main = document.getElementById('main');
+if (8 > t) {
+    t += 1;
+} else {
+    main.removeChild(document.getElementById('main').firstChild);
+}
+last = document.createElement('div');
+last.innerHTML = stanza();
+main.appendChild(last);
+}
+function produce_litany() {
+"use strict";
+litany();
+setInterval(litany, 5000);
+}
