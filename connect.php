4f9ffc70dae4ff84faeeddb9371c86fbc6ab164e
lahdaeyoung <lahydaeyoung@gmail.com>
2022-05-23 16:41:58 -0400

    fixing php

--- connect.php
+++ connect.php
@@ -2,9 +2,9 @@
     //server info and log-in details
     //keep servername the same and change to your username, password, and database name
     $servername = "localhost:3308";
-    $username = "web06dbuser";
-    $password = "SP2022web3985";
-    $dbname = "web06db";
+    $username = "webp2";
+    $password = "Web22web331";
+    $dbname = "webp2";
     
     //create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -16,12 +16,11 @@
     echo "connected successfully<br>";
 
     //pull form data and store in variables
-    if (isset($_POST['lik'], $_POST['sav'], $_POST['for'], $_POST['hear'], $_POST['suggestions'])) {
-        $lik = $_POST['lik'];
-        $sav = $_POST['sav'];
-        $hear = $_POST['hear'];
-        $for = $_POST['for'];
-        $suggestions = $_POST['suggestions'];
+    if (isset($_POST['firstname'], $_POST['lastname'], $_POST['class'], $_POST['year'])) {
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $class = $_POST['class'];
+        $year = $_POST['year'];
 
     } else {
         echo 'You need to input all the details.';
@@ -29,15 +28,14 @@
 
     //inserting form data into a table in your database
     //student is the name of the table i created and firstname, lastname, class, year are the names of my columns
-
-    $sql = "INSERT INTO imagedata (lik, sav, for, hear, suggestions)
-    VALUES ('$lik', '$sav', '$hear', '$for', '$suggestions')";
+    $sql = "INSERT INTO students (firstname, lastname, class, year)
+    VALUES ('$firstname', '$lastname', '$class', '$year')";
     
     //return status to user
     if (mysqli_query($conn, $sql)) {
-        echo "Thank you for your response! ";
+        echo "New record created successfully";
       } else {
-        echo "Please Try Again.: " . $sql . "<br>" . mysqli_error($conn);
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
       }
 
     //close connection
