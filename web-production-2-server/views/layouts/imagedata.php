3fc4fd5ea93c9dba86f7c1863f3c90ebff471d09
lahdaeyoung <lahydaeyoung@gmail.com>
2022-05-23 20:11:28 -0400

    make it better

---
+++ web-production-2-server/views/layouts/imagedata.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.9.2
+ */
+
+/**
+ * Database `web06db`
+ */
+
+/* `web06db`.`clothingdata` */
+$imagedata = array(
+  array('lik' => '0','Price' => '0','Category' => '','Color' => '0','Size' => '0'),
+  array('sav' => '0','Price' => '0','Category' => '','Color' => '0','Size' => '0'),
+  array('ID' => '0','Price' => '0','Category' => '','Color' => '0','Size' => '0'),
+  array('ID' => '0','Price' => '0','Category' => '','Color' => '0','Size' => '0')
+);
